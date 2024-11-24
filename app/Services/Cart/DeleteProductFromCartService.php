5f39a1e54daeafea9ca9785d98ccc7cf66d9fdd5
olegbugaev <bugaev0leg@yandex.ru>
2024-11-24 20:07:01 +0400

    feat: add service DeleteProductFromCartService

---
+++ app/Services/Cart/DeleteProductFromCartService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Cart;
+
+use App\Models\Cart;
+use App\Models\CartProduct;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class DeleteProductFromCartService
+{
+    public function deleteProductFromCart(array $data): void
+    {
+        Auth::setUser(User::query()->first());
+        $cart = Cart::query()->where('user_id', '=', Auth::user()->id)->first();
+        $cartProduct = CartProduct::query()->where('cart_id', '=', $cart->id)->first();
+
+        if ($cart !== null && $cartProduct->quantity === 1) {
+            CartProduct::query()
+                ->where('cart_id', '=', $cart->id)
+                ->where('product_id', '=', $data['product_id'])
+                ->delete();
+        } elseif ($cart !== null && $cartProduct->quantity > 1) {
+            $cartProduct = CartProduct::query()
+                ->where('cart_id', '=', $cart->id)
+                ->where('product_id', '=', $data['product_id'])
+                ->first();
+
+            $cartProduct->quantity -= $data['product_id'];
+            $cartProduct->save();
+
+            if ($cartProduct->quantity < 1) {
+                $cartProduct->delete();
+            }
+        }
+    }
+}
