843bfbde0f49f17156d33c646eae053049cde485
olegbugaev <bugaev0leg@yandex.ru>
2024-11-24 20:07:40 +0400

    feat: add new route

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('products')->group(function () {
+    Route::post('/add-product-to-cart', [CartController::class, 'addProductToCart']);
+    Route::post('/delete-product-from-cart', [CartController::class, 'deleteProductFromCart']);
+    Route::get('/{id}', [CartController::class, 'getProduct']);
+    Route::get('/', [CartController::class, 'getProducts']);
+});
+
